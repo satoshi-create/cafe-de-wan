c44b57ebde3c1734b9c075c90b342eba63978749
satoshi <mimizunobouken@gmail.com>
2021-11-05 00:28:14 +0900

    add Nav,Footer components

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,36 @@
 import React from 'react'
+import styled from 'styled-components'
+import nikukyuWhite from "../images/nikukyu-white.svg";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <p>©2021 cafe de WON all right reserved</p>
-      <img alt="nikukyu-icon" />
-    </footer>
+    <Wrapper className="partsGrid">
+      <div className="grid12">
+        <p>©2021 cafe de WON all right reserved</p>
+        <img src={nikukyuWhite} alt="nikukyu-white" />
+      </div>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.footer`
+background-color: var(--clr-black);
+height: 224px;
+  p{
+    color:var(--clr-white);
+    font-size: 13px;
+    align-self: end;
+    justify-self: center;
+  }
+  img{
+    justify-self: right;
+    align-self: flex-start;
+    @media (min-width: 768px) {
+      grid-column: 9/10;
+  }
+}
+`
+
+
 
 export default Footer
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,61 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import logo from "../images/logo.svg";
+import styled from 'styled-components'
 
 const Navbar = () => {
   return (
-    <nav className="nav">
-      <img src={logo} alt="logo" />
-      <Link to="/">
-        <button className="btn nav-icon">nav</button>
-      </Link>
-      <ul className="nav-menu">
-        <li>
-          <Link to="/">ホーム</Link>
-        </li>
-        <li>
-          <Link to="/">ブログ</Link>
-        </li>
-        <li>
-          <Link to="/">お問い合わせ</Link>
-        </li>
-      </ul>
-    </nav>
+    <Wrapper className="partsGrid">
+      <nav className="nav grid12">
+        <img src={logo} alt="logo" className="nav-logo" />
+        <Link to="/" className="btn nav-icon">
+          <button>nav</button>
+        </Link>
+        <ul className="nav-menu">
+          <li>
+            <Link to="/">ホーム</Link>
+          </li>
+          <li>
+            <Link to="/">ブログ</Link>
+          </li>
+          <li>
+            <Link to="/">お問い合わせ</Link>
+          </li>
+        </ul>
+      </nav>
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.nav`
+.nav{
+  place-items: center;
+  padding: 30px 0;
+  .nav-logo{
+    grid-column: span 1;
+  }
+  .nav-icon{
+    grid-column: 4/-1;
+    @media (min-width: 768px) {
+      display: none;
+  }
+  }
+  .nav-menu{
+    display: none;
+    @media (min-width: 768px) {
+      display: block;
+      display: grid;
+      grid-template-columns: repeat(3,auto);
+      grid-column-gap: 20px;
+      grid-column: spna 4 /-1;
+      a{
+        font-family: var(--subtitle-font);
+        font-size: 14px;
+      }
+  }
+  }
+}
+`
+
 
 export default Navbar
