7b06dad2e1034b84195a175f67602f02957285df
satoshi <mimizunobouken@gmail.com>
2021-11-30 14:41:10 +0900

    add gatsby-plugin-manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,17 +1,18 @@
 module.exports = {
   siteMetadata: {
     title: "cafe de wan",
-    description:"犬と一緒にカフェを運営しています",
-    lang:"ja",
-    url:"https://goofy-neumann-595c81.netlify.app/"
-    
+    description: "犬と一緒にカフェを運営しています",
+    lang: "ja",
+    url: "https://goofy-neumann-595c81.netlify.app/",
   },
   plugins: [
     `gatsby-plugin-image`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-styled-components`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
@@ -19,5 +20,38 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `cafe de wan`,
+        short_name: `cafe de wan`,
+        start_url: `/`,
+        background_color: `#fff7e2`,
+        theme_color: `#fff7e2`,
+        display: `standalone`,
+        icon: `src/images/logo.svg`,
+      },
+    },
+    // {
+    //   resolve: `gatsby-plugin-webfonts`,
+    //   options: {
+    //     fonts: {
+    //       google: [
+    //         {
+    //           family: "Dancing Script",
+    //           variants: ["700"],
+    //         },
+    //         {
+    //           family: "Josefin Sans",
+    //           variants: ["700"],
+    //         },
+    //         {
+    //           family: "Noto Sans JP",
+    //           variants: ["400"],
+    //         },
+    //       ],
+    //     },
+    //   },
+    // },
   ],
 };
